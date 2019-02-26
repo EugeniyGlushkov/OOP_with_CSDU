63b69d256bb6929f437601ab18121c0c11633cfa
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-02-26 17:24:23 +0300

    git commit -m IntStr

---
+++ src/main/java/ru/alvisid/week_2/IntAssignmentTester.java
@@ -0,0 +1,12 @@
+package main.java.ru.alvisid.week_2;
+
+public class IntAssignmentTester {
+
+	public static void main(String[] args) {
+		int var1 = 17;
+		int var2 = var1 + 1;
+		var1 = var2 + 1;
+		System.out.println("var1: " + var1 + " var2: " + var2);
+	}
+
+}
