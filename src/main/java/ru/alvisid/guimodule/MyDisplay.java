ac1085050744d1ec6ee09c9c27997279f46948ca
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-03-05 08:31:25 +0300

    Sun sec

--- src/main/java/ru/alvisid/guimodule/MyDisplay.java
+++ src/main/java/ru/alvisid/guimodule/MyDisplay.java
@@ -5,10 +5,10 @@ import processing.core.PImage;
 
 public class MyDisplay extends PApplet {
 	private PImage img;
-	
+
 	@Override
 	public void settings() {
-		size(400, 400);		
+		size(400, 400);
 	}
 
 	@Override
@@ -16,15 +16,30 @@ public class MyDisplay extends PApplet {
 		background(255);
 		stroke(0);
 		img = loadImage("http://cseweb.ucsd.edu/~minnes/palmTrees.jpg", "jpg");
-		img.resize(0,  400);
+		img.resize(0, 400);
 		image(img, 0, 0);
 	}
-	
+
 	@Override
 	public void draw() {
-		ellipse(width/4, height/5, width/4, height/5);
+		int[] colorsRGB = sunColorSec(second());
+		fill(colorsRGB[0], colorsRGB[1], colorsRGB[2]);
+		ellipse(width / 4, height / 5, width / 4, height / 5);
 	}
-	
+
+	public int[] sunColorSec(int second) {
+		int red, green;
+		int blue = 0;
+
+		if (second <= 30) {
+			red = green = 255 / 30 * second;			
+		} else {
+			red = green = 255 / 30 * (60 - second);
+		}
+
+		return new int[] {red, green, blue};
+	}
+
 	public static void main(String[] args) {
 		PApplet.main(MyDisplay.class.getName());
 	}
