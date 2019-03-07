e0caf03290353cca064eb30e3403634c7376a243
EugeniyGlushkov <eugeniyglushkov@gmail.com>
2019-03-07 14:32:39 +0300

    Add custom markers

--- src/main/java/ru/alvisid/citymaps/earthquake/EarthquakeCityMap.java
+++ src/main/java/ru/alvisid/citymaps/earthquake/EarthquakeCityMap.java
@@ -11,30 +11,32 @@ import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 public class EarthquakeCityMap extends PApplet {
 	private UnfoldingMap map;
 
-    public void settings() {
-        size(950, 600, P3D);
-    }
+	public void settings() {
+		size(950, 600, P3D);
+	}
 
-    public void setup() {
-        //map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
-    	map = new UnfoldingMap(this, 200, 50, 700, 500, new Microsoft.HybridProvider());
+	public void setup() {
+		map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
+		// map = new UnfoldingMap(this, 200, 50, 700, 500, new
+		// Microsoft.HybridProvider());
 		map.zoomToLevel(2);
-        MapUtils.createDefaultEventDispatcher(this, map); 
-        Location valLoc = new Location(-38.14f, -73.03f);
-        SimplePointMarker val = new SimplePointMarker(valLoc);
-        map.addMarker(val);
-    }
+		MapUtils.createDefaultEventDispatcher(this, map);
+		Location valLoc = new Location(-38.14f, -73.03f);
+		// SimplePointMarker val = new SimplePointMarker(valLoc);
+		SimplePointMarker val = new MarkerMiddleMagnitude(valLoc);
+		map.addMarker(val);
+	}
 
-    public void draw() {  
-		background(220);      
-        map.draw();
+	public void draw() {
+		background(220);
+		map.draw();
 		addKey();
-    }  
-	
+	}
+
 	public static void main(String[] args) {
 		PApplet.main(EarthquakeCityMap.class.getName());
 	}
-	
+
 	private void addKey() {
 		fill(255);
 		rect(25, 50, 150, 300, 5);
@@ -51,4 +53,53 @@ public class EarthquakeCityMap extends PApplet {
 		fill(0, 0, 255);
 		circle(50, 260, 6);
 	}
+
+	/**
+	 * Strong magnitude marker, shows earthquake is 5.0+ magnitude.
+	 * 
+	 * @author evgen glushkov
+	 * @since 2019.03.07
+	 * @see EarthquakeCityMap#MarkerMiddleMagnitude.
+	 * @see EarthquakeCityMap#MarkerLowMagnitude.
+	 */
+	public class MarkerStrongMagnitude extends SimplePointMarker {
+		public MarkerStrongMagnitude(Location location) {
+			super(location);
+			setColor(color(255, 0, 0));
+			setDiameter(18);
+		}
+	}
+
+	/**
+	 * Middle magnitude marker, shows earthquake is 4.0 to 5.0 magnitude.
+	 * 
+	 * @author evgen glushkov
+	 * @since 2019.03.07
+	 * @see EarthquakeCityMap#MarkerStrongMagnitude.
+	 * @see EarthquakeCityMap#MarkerLowMagnitude.
+	 */
+	public class MarkerMiddleMagnitude extends SimplePointMarker {
+		public MarkerMiddleMagnitude(Location location) {
+			super(location);
+			setColor(color(255, 255, 0));
+			setDiameter(10);
+		}
+	}
+
+	/**
+	 * Low magnitude marker, shows earthquake is below 4.0 magnitude.
+	 * 
+	 * @author evgen glushkov
+	 * @since 2019.03.07
+	 * @see EarthquakeCityMap#MarkerStrongMagnitude.
+	 * @see EarthquakeCityMap#MarkerMiddleMagnitude.
+	 */
+	public class MarkerLowMagnitude extends SimplePointMarker {
+		public MarkerLowMagnitude(Location location) {
+			super(location);
+			setColor(color(0, 0, 255));
+			setDiameter(6);
+		}
+	}
+
 }
